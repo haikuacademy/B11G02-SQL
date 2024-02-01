d3e2f0af15de9fc9f8141a59f4b2fbcd073791d7
fergiegorospe <146056111+fergiegorospe@users.noreply.github.com>
2024-02-01 15:13:23 +0700

    updated joins with bookings

--- bookings.sql
+++ bookings.sql
@@ -6,9 +6,8 @@ VALUES (5, 5, 5, '2024-01-31', '2024-02-10', 25, LOCALTIMESTAMP);
 
 -- READ BOOKING
 
-SELECT location, bookings.check_in, bookings.check_out, price_per_night, bookings.total_price, bedroom, users.first_name, users.user_id, houses_pictures.photo_url
-FROM houses
-JOIN bookings ON bookings.house_id = houses.house_id
-JOIN users ON bookings.user_id = users.user_id
+SELECT location, bookings.check_in, bookings.check_out, price_per_night, bookings.total_price, bedroom, bookings.user_id, houses_pictures.photo_url
+FROM bookings
+JOIN houses ON bookings.house_id = houses.house_id
 JOIN houses_pictures ON houses.house_id = houses_pictures.house_id
-WHERE users.user_id = 1;
+WHERE bookings.user_id = 1;
