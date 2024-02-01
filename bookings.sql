7e458e9f9cc4fb3fbd58b8f026cbbeb33801ba30
fergiegorospe <146056111+fergiegorospe@users.noreply.github.com>
2024-02-01 14:58:29 +0700

    updated booking queries with user and houses joins

--- bookings.sql
+++ bookings.sql
@@ -1,13 +1,14 @@
 
 -- CREATE BOOKING
 
-INSERT INTO bookings (booking_id, user_id, house_id, check_in, check_out, total_price, booking_status, payment_method, payment_status, booked_on)
-VALUES (5, 5, 5, '2024-01-31', '2024-02-10', 25, 'confirmed', 'cash', 'paid', LOCALTIMESTAMP);
+INSERT INTO bookings (booking_id, user_id, house_id, check_in, check_out, total_price, booked_on)
+VALUES (5, 5, 5, '2024-01-31', '2024-02-10', 25, LOCALTIMESTAMP);
 
 -- READ BOOKING
 
-SELECT location, bookings.check_in, bookings.check_out, price_per_night, bookings.total_price, bedroom,  bathroom 
+SELECT location, bookings.check_in, bookings.check_out, price_per_night, bookings.total_price, bedroom, users.first_name, users.user_id, houses_pictures.photo_url
 FROM houses
-JOIN bookings
-ON bookings.booking_id = houses.house_id
-WHERE location = 'Bangkok' OR location = 'Bali';
+JOIN bookings ON bookings.house_id = houses.house_id
+JOIN users ON bookings.user_id = users.user_id
+JOIN houses_pictures ON houses.house_id = houses_pictures.house_id
+WHERE users.user_id = 1;
