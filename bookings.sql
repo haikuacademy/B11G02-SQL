86c4708df124f736c56266b478074246d1650c31
Emiliano <onorati@gmail.com>
2024-02-01 14:04:42 +0700

    delete invisible characters

--- bookings.sql
+++ bookings.sql
@@ -6,7 +6,8 @@ VALUES (5, 5, 5, LOCALTIMESTAMP, LOCALTIMESTAMP, 25, 'confirmed', 'cash', 'paid'
 
 -- READ BOOKING
 
-SELECT location, bookings.check_in, bookings.check_out, price_per_night﻿﻿, bookings.total_price, bedroom﻿,  bathroom﻿ FROM houses
+SELECT location, bookings.check_in, bookings.check_out, price_per_night, bookings.total_price, bedroom,  bathroom 
+FROM houses
 JOIN bookings
 ON bookings.booking_id = houses.house_id﻿
 WHERE location = 'Bangkok' OR location = 'Bali';
