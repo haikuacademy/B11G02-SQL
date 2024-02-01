7a1d7b3743964f714de2eef740f19a9652d932d4
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-01 15:17:21 +0700

    deleted the houses_list file and combine it with the houses file

--- houses.sql
+++ houses.sql
@@ -17,3 +17,14 @@ WHERE house_id = 2
 -- Query to Delete a house 
 DELETE FROM houses
 WHERE house_id = 2
+
+-- This will select all the information about every house
+SELECT houses_pictures.photo_url, houses.location, houses.bedroom, houses.bathroom, houses.price_per_night
+FROM houses_pictures
+INNER JOIN houses ON houses_pictures.house_id = houses.house_id
+INNER JOIN reviews ON houses.house_id = reviews.house_id
+
+-- This will count the total amount of reviews that each property has
+SELECT COUNT(reviews.review) FROM reviews
+INNER JOIN houses ON reviews.house_id = houses.house_id
+WHERE houses.house_id = 1
