23757776f21b180be048e2e1841ac5de188ee7cf
fergiegorospe <146056111+fergiegorospe@users.noreply.github.com>
2024-02-01 14:00:10 +0700

    bookings queries created

---
+++ bookings.sql
@@ -0,0 +1,12 @@
+
+-- CREATE BOOKING
+
+INSERT INTO bookings (booking_id, user_id, house_id, check_in, check_out, total_price, booking_status, payment_method, payment_status, booked_on)
+VALUES (5, 5, 5, LOCALTIMESTAMP, LOCALTIMESTAMP, 25, 'confirmed', 'cash', 'paid', LOCALTIMESTAMP);
+
+-- READ BOOKING
+
+SELECT location, bookings.check_in, bookings.check_out, price_per_night﻿﻿, bookings.total_price, bedroom﻿,  bathroom﻿ FROM houses
+JOIN bookings
+ON bookings.booking_id = houses.house_id﻿
+WHERE location = 'Bangkok' OR location = 'Bali';
