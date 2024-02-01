15608e1e228ba66a413253f34b43761164a7416f
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-01 15:38:52 +0700

    quieries for display a list of pictures where corrected and added a new query to list a new property

--- houses.sql
+++ houses.sql
@@ -22,7 +22,7 @@ WHERE house_id = 2
 SELECT houses_pictures.photo_url, houses.location, houses.bedroom, houses.bathroom, houses.price_per_night
 FROM houses_pictures
 INNER JOIN houses ON houses_pictures.house_id = houses.house_id
-INNER JOIN reviews ON houses.house_id = reviews.house_id
+WHERE houses.house_id = 3
 
 -- This will count the total amount of reviews that each property has
 SELECT COUNT(reviews.review) FROM reviews
--- houses_photos.sql
+++ houses_photos.sql
@@ -1,3 +1,7 @@
+-- This will insert a new picture of the property
+INSERT INTO houses_pictures (photo_url, house_id)
+VALUES ('https://randomuser.me/api/portraits/men/12.jpg', 1)
+
 -- This will update the photos of the houses
 UPDATE houses_pictures 
 SET photo_url = 'https://randomuser.me/api/portraits/women/27.jpg'
